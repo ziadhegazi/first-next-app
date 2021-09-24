9460c4711723e8595a8f23e6973ebe4fc1de8881
Ziad Hegazi <ziadhegazi@hotmail.com>
2021-09-24 13:24:34 +0200

    redirecting users

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/");
+        }, 3000);
+    }, []);
+
     return (
         <div className="not-found">
             <h1>Ooooops...</h1>
