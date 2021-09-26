1b13f465e554db9255f96f21063d5ecf38e14ab5
Ziad Hegazi <ziadhegazi@hotmail.com>
2021-09-26 15:55:21 +0200

    dynamic routes - part1

--- pages/people/index.jsx
+++ pages/people/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/dist/shared/lib/head";
 import axios from "axios";
 import styles from "../../styles/People.module.css";
+import Link from "next/dist/client/link";
 
 export const getStaticProps = async () => {
     const response = await axios.get(
@@ -25,11 +26,11 @@ const People = ({ People }) => {
             <div>
                 <h1>All people</h1>
                 {People.map((person) => (
-                    <div key={person.id}>
+                    <Link href={`/people/${person.id}`} key={person.id}>
                         <a className={single}>
                             <h3>{person.name}</h3>
                         </a>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </>
